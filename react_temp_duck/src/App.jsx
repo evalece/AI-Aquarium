97b4e04ea10d7d6a6d5d70410fde2ef4697e1b60
evalece <60049803+evalece@users.noreply.github.com>
2025-08-14 16:51:11 -0400

    Controlled Component

--- react_temp_duck/src/App.jsx
+++ react_temp_duck/src/App.jsx
@@ -3,36 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useRef, useState } from "react";
+// 3. Controlled Component 
+// Render <-> DOM Caching 
+// useState to store values, and DOM events (i.e., onChange) to update state 
+// Hanlders best be at child level for min. re-render, though, state and function may come from parent 
 
+// Enforce DOM update into memory update without re-render
+//setState re-renders if not === ; hence, use callback function, allow dependency=[]
+
+//Enforcer re-render (but advance)
+// useReducer
+
+//Interactivity- Rather fine chopping DOM for mini-update for better UX.
 function App() {
-// 2. Child -> Parent / AKA. Lift State Up
-// Except useEffect, callback, hanlder or custom hook, 
-// child use interactive HTML tag, hook parent handler as its props 
 
-// when parent calls child in its JSX like...
-// <Child xxx=ooo> child only sees xxx as "props that is passed to it"
-// Summary in Parent => Child and Child => Parent, match xxx from either way. 
-  function Parent() {
-    function clickHandler(){
-      console.log("clicked");
-    }
-    return (<Children hanldeClick={()=>clickHandler()}  />);
- 
-  }
-  function Children({hanldeClick}){
-    return (<div>
-    
-      <button onClick={hanldeClick}>
-        Click Me  
+// user input-> setInput (update state) -> React re-render (hence having new input in value)-> assign to 
+// input value field for displaying
 
-      </button>
-      </div>
-    );
+//onXXX => has a set of Attributes (advanced), if not used, omit:
+  // onClick={(e) => setCount(count + 1)}
+  // onClick={() => setCount(count + 1)}
+  // onClick={handleClick}
+function Parent(){
+  const [input, SetInput]=useState("");
+  return(
+    <div>
+      <div> your input: {input}</div>
+      <input value={input}  
+      onChange= {(e) => setInput(e.target.value)} />
 
-  }
-
-  
-
+    </div>
+  );
+}
   
  return (< Parent/>);
 
