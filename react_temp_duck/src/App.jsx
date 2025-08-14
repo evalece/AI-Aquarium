e88024163a5ae32b678530ff5039fdbff3bb3655
evalece <60049803+evalece@users.noreply.github.com>
2025-08-14 16:01:03 -0400

    (Topic: Passing values/ props) child->parent and c->p; p->c summary

--- react_temp_duck/src/App.jsx
+++ react_temp_duck/src/App.jsx
@@ -3,19 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useRef, useState } from "react";
-/*
-value={text} → render from state
-onChange={(e) => setText(e.target.value)} → update state
-*/
+
 function App() {
+// 2. Child -> Parent / AKA. Lift State Up
+// Except useEffect, callback, hanlder or custom hook, 
+// child use interactive HTML tag, hook parent handler as its props 
 
+// when parent calls child in its JSX like...
+// <Child xxx=ooo> child only sees xxx as "props that is passed to it"
+// Summary in Parent => Child and Child => Parent, match xxx from either way. 
   function Parent() {
-    return (<Children value={2} />);
-
+    function clickHandler(){
+      console.log("clicked");
+    }
+    return (<Children hanldeClick={()=>clickHandler()}  />);
+ 
   }
-  function Children({value}){
-    return <h1>Child contains value={value}</h1>;
+  function Children({hanldeClick}){
+    return (<div>
     
+      <button onClick={hanldeClick}>
+        Click Me  
+
+      </button>
+      </div>
+    );
+
   }
 
   
