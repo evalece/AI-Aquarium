bb7621e09584fa6e7d27519c182fde8249d7e370
evalece <60049803+evalece@users.noreply.github.com>
2025-08-14 14:51:20 -0400

    (Topic: Passing values/ props) parent -> child

--- react_temp_duck/src/App.jsx
+++ react_temp_duck/src/App.jsx
@@ -8,38 +8,20 @@ value={text} → render from state
 onChange={(e) => setText(e.target.value)} → update state
 */
 function App() {
-  
-  // user input for temperature list for hysteresis testing 
-  const temperaturesRef = useRef([]);
-  const [currentInput, SetCurrentInput]=useState(""); 
 
-  function handleAdd(e){
-      e.preventDefault(); // stop form submit from reloading the page
-      const v = parseFloat(currentInput);
-      if (Number.isNaN(v)) return; // ignore invalid input
-      temperaturesRef.current.push(v);
-      setCurrentInput("");  
+  function Parent() {
+    return (<Children value={2} />);
+
   }
-  function TestTemp(){ // capture latest DOM input before user presses submit
-    return (
-      <div>
-      <form onSubmit={handleAdd}>
-        <label> Input Temperature Series for Hysteresis Testing
-          <input type ="text" 
-          value={currentInput}
-          onChange={(e) => SetCurrentInput(e.target.value)  
-          }
-          />
-            </label>
-        <button type='submit'> 
-          Submit/ Add 
-        </button>
-      
-      </form>
-      </div>
-    );
+  function Children({value}){
+    return <h1>Child contains value={value}</h1>;
+    
   }
- return <TestTemp />;
+
+  
+
+  
+ return (< Parent/>);
 
 }
 
